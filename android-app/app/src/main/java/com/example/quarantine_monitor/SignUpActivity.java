6d542ef757714ca9a9e3f770c7b764f984dd67f1
nchen99 <nathanielle.chen@gmail.com>
2021-03-25 13:20:31 -0700

    fixed location issue

--- android-app/app/src/main/java/com/example/quarantine_monitor/SignUpActivity.java
+++ android-app/app/src/main/java/com/example/quarantine_monitor/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.example.quarantine_monitor;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -47,6 +48,7 @@ public class SignUpActivity extends AppCompatActivity implements LocationListene
     private EditText usernameText;
     private EditText passwordText;
     private Button signUpButton;
+    private LocationManager locationManager;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -63,7 +65,7 @@ public class SignUpActivity extends AppCompatActivity implements LocationListene
         passwordText = (EditText) findViewById(R.id.input_password);
         coordinates = new Double [2];
 
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -98,7 +100,7 @@ public class SignUpActivity extends AppCompatActivity implements LocationListene
                 .setMessage(message)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        requestPermission(permissions, permissionCode);
+                        requestPermissions(permissions, permissionCode);
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -113,11 +115,25 @@ public class SignUpActivity extends AppCompatActivity implements LocationListene
     }
 
     /*
-     * @desc: this function requests the location permissions from the user.
+     * @desc: this function is called after permissions have been granted. It shows toasts based
+     * on what has happened.
      * */
-    private void requestPermission(String[] permissions, int permissionRequestCode) {
-        ActivityCompat.requestPermissions(this,
-                permissions, permissionRequestCode);
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_LOCATION:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(SignUpActivity.this, "Permission Granted, Thank You!", Toast.LENGTH_SHORT).show();
+                    locationManager.requestLocationUpdates(GPS_PROVIDER, 0, 0, this);
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     private void signUp(){
