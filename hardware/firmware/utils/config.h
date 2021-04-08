ba5e06f10034f62f0f6356f65ea8a186d1c20c43
lcfyi <leslie.cheng5@gmail.com>
2021-04-08 15:06:26 -0700

    Update settings location.

--- hardware/firmware/utils/config.h
+++ hardware/firmware/utils/config.h
@@ -12,7 +12,7 @@ public:
     std::string get_value(std::string section, std::string key);
     void set_value(std::string section, std::string key, std::string value);
 
-    std::string SETTINGS_CACHE_FILE = "/var/cache/settings.ini";
+    std::string SETTINGS_CACHE_FILE = "/home/root/settings.ini";
     std::string WIFI_SETTINGS = "wifi";
     std::string ACCELEROMETER_SETTINGS = "accel";
     std::string SERVER_SETTINGS = "server";
